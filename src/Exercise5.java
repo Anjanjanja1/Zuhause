345b22173a472fee794750e5f1c2968a9cdd8dc7
anjaharare <anja.frntic@gmail.com>
2024-03-02 14:16:08 +0100

    changes 1.03.

---
+++ src/Exercise5.java
@@ -0,0 +1,18 @@
+public class Exercise5 {
+    public static void main(String[] args) {
+        Fibonacci();
+    }
+
+    static void Fibonacci() {
+        int broj1 = 0;
+        int broj2 = 1;
+        int sum = 0;
+        while (true) {
+            for (int i = 0; i <= 20; i++) {
+                sum = sum + broj1 + broj2;
+                sum = broj2;
+
+            }
+        }
+    }
+}
