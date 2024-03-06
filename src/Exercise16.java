8f2458a0d24c80f9e7562d40b6071b7e5986610a
anjaharare <anja.frntic@gmail.com>
2024-03-06 15:52:15 +0100

    na faksu

--- src/Exercise16.java
+++ src/Exercise16.java
@@ -1,6 +1,7 @@
 //Write a program that can convert Celsius to Fahrenheit and vice versa.
 // The conversions should be outsourced to methods. The value to be converted and whether it should be
 // converted to Fahrenheit or Celsius should be entered by the user.
+//"%.2f" is for shorter decimal number!
 
 import java.util.Scanner;
 
@@ -13,22 +14,34 @@ public class Exercise16 {
 
     public static void Inp() {
         System.out.print("Enter degrees: ");
-        int d = input.nextInt();
+        double d = input.nextDouble();
         String Dee;
         do {
             System.out.println("F oder C?");
             Dee = input.next().toUpperCase();
             if (Dee.equals("F")) {
-                //METODA
+                FtoC(d);
+                return;
             } else if (Dee.equals("C")) {
-                //METODA
+                CtoF(d);
+                return;
             } else {
                 System.out.println("Did you mistype?");
             }
         } while (!(Dee.equals("F") || Dee.equals("C")));
 
     }
-    public static void Calc(int d, String Dee){
 
-            }
+    public static void FtoC(double d) {
+        double cel= ((d - 32) / 1.8);
+        System.out.println(d+" Fahrenheit is "+cel+" Celsius.");
+        Inp();
+    }
+
+    public static void CtoF(double d) {
+        double fah= ((d * 1.8) + 32);
+        System.out.println(d+" Celsius is "+fah+" Fahrenheit.");
+        Inp();
+    }
+
 }
